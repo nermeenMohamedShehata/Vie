0326047e71c81c7aadb15077b78dc5ea01420855
Nerneen Mohamed <nermeen@enhance.online>
2019-01-05 23:28:51 +0200

    Container

---
+++ Vie/Controllers/Container List/HomeContainerVC.swift
@@ -0,0 +1,33 @@
+//
+//  HomeContainerVC.swift
+//  Vie
+//
+//  Created by Nermeen Mohamed on 1/5/19.
+//  Copyright © 2019 Nermeen Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class HomeContainerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad() 
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapChangeDisplayMode(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
